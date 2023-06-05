0ec0b6a67ecd4b379ccffef98bd23ec226d32c63
Mansy <abdomansy19@gmail.com>
2023-06-05 14:37:20 +0300

    Sales Analysis I

---
+++ LeetCode/LeetCode 1082.sql
@@ -0,0 +1,14 @@
+-- Sales analysis I
+
+--table1 (Product) columns (product_id, product_name,unit_price)
+--table2 (Sales) columns (seller_id, product_id, buyer_id, sale_date, quantity, price)
+
+SELECT saller_id
+FROM Sales
+GROUP BY saller_id
+HAVING SUM(price) = (
+SELECT TOP 1 SUM(price) AS total_sales_amount
+FROM Sales
+WHERE rownum = 1
+GROUP BY saller_id
+ORDER BY total_sales_amount DESC)
