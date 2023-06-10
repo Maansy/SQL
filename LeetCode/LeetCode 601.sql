41c46f56a88d9686df82cae28cdf1a95b595adbd
Mansy <abdomansy19@gmail.com>
2023-06-10 16:26:09 +0300

    Human traffic of stadium

---
+++ LeetCode/LeetCode 601.sql
@@ -0,0 +1,13 @@
+--Human traffic of stadium
+
+--table 1 Staduim columns (id , visit_date 'PK',people)
+
+SELECT DISTINCT s1.*
+FROM Staduim AS s1
+JOIN Staduim AS s2
+JOIN Staduim AS s3
+ON (s1.id = s2.id -1 AND s1.id = s3.id-2) OR
+(s1.id = s2.id +1 AND s1.id = s3.id-1) OR
+(s1.id = s2.id +1 AND s1.id = s3.id+2)
+WHERE s1.people >= 100 AND s2.people >= 100 AND s3.people >= 100
+ORDER BY visit_date 
